b9034afbf6ce2c8341bec10948eddb5cd7034fd7
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-04-25 04:54:50 +0400

    Update benchmarks

--- benchmarks/Benchmarks.hs
+++ benchmarks/Benchmarks.hs
@@ -10,44 +10,65 @@ import qualified Data.HashTable.IO as T
 main = do
   gen <- R.newGen
   C.benchmark $ do
-    C.standoff "Table" $ do
+
+    C.standoff "VS Hashtables" $ do
       rows <- liftIO $ replicateM 10000 $ 
         (,) <$> R.generateName gen <*> R.generateVariate gen
       liftIO $ evaluate $! rnf rows
-      C.group "Hashtables" $ do
-        C.subject "10000" $ do
-          liftIO $ do
-            t :: T.LinearHashTable StrictText Int <- T.new
-            forM_ rows $ uncurry $ T.insert t
-      C.group "HashtablesPlus" $ do
-        C.subject "10000" $ do
-          liftIO $ do
-            t :: H.Table H.Linear StrictText Int <- H.new
-            forM_ rows $ H.insertFast t
+      C.subject "HashtablesPlus" $ do
+        liftIO $ do
+          t :: H.Table H.Linear StrictText Int <- H.new
+          forM_ rows $ H.insertFast t
+      C.subject "Hashtables" $ do
+        liftIO $ do
+          t :: T.LinearHashTable StrictText Int <- T.new
+          forM_ rows $ uncurry $ T.insert t
+
+    C.standoff "Set/Inserting" $ do
+      let 
+        subject :: 
+          forall c. (H.Insert c, H.Row c ~ StrictText) => 
+          c -> Int -> C.Standoff ()
+        subject t n = do
+          values <- liftIO $ replicateM n $ R.generateName gen
+          C.subject (cs $ show n) $ do
+            liftIO $ do
+              c :: c <- H.new
+              forM_ values $ H.insertFast c
+
+      C.group "Sized" $ do
+        C.group "Linear" $ do
+          mapM_ (subject (undefined :: H.Set H.Linear StrictText)) 
+                [10000, 9000, 8000]
+      C.group "Linear" $ do
+        mapM_ (subject (undefined :: H.Set H.Linear StrictText)) 
+              [10000, 9000, 8000]
+      C.group "Cuckoo" $ do
+        mapM_ (subject (undefined :: H.Set H.Cuckoo StrictText)) 
+              [10000, 9000, 8000]
+      C.group "Basic" $ do
+        mapM_ (subject (undefined :: H.Set H.Basic StrictText)) 
+              [10000, 9000, 8000]
+
+    C.standoff "Multitable/Inserts" $ do
+      rows <- 
+        liftIO $ forM [10000, 9000, 8000] $ \n -> do
+          replicateM n $ (,) <$> R.generateName gen <*> R.generateVariate gen
+      liftIO $! evaluate $! rnf $! rows
+      C.group "Multitable" $ do
+        C.group "Linear" $ do
+          forM_ rows (rowsInsertionSubject (undefined :: H.MultiTable H.Linear StrictText (H.Set H.Linear Int)))
+      C.group "Table" $ do
+        C.group "Linear" $ do
+          forM_ rows (rowsInsertionSubject (undefined :: H.Table H.Linear StrictText Int))
 
 
-    -- C.standoff "Set/Inserting" $ do
-    --   let 
-    --     subject :: 
-    --       forall c. (H.Insert c, H.Row c ~ StrictText) => 
-    --       c -> Int -> C.Standoff ()
-    --     subject t n = do
-    --       values <- liftIO $ replicateM n $ R.generateName gen
-    --       C.subject (cs $ show n) $ do
-    --         liftIO $ do
-    --           c :: c <- H.new
-    --           forM_ values $ H.insert c
 
-    --   C.group "Sized" $ do
-    --     C.group "Linear" $ do
-    --       mapM_ (subject (undefined :: H.Set H.Linear StrictText)) 
-    --             [31000, 26000, 21000, 16000, 11000, 6000, 1000]
-    --   C.group "Linear" $ do
-    --     mapM_ (subject (undefined :: H.Set H.Linear StrictText)) 
-    --           [31000, 26000, 21000, 16000, 11000, 6000, 1000]
-    --   C.group "Cuckoo" $ do
-    --     mapM_ (subject (undefined :: H.Set H.Cuckoo StrictText)) 
-    --           [31000, 26000, 21000, 16000, 11000, 6000, 1000]
-    --   C.group "Basic" $ do
-    --     mapM_ (subject (undefined :: H.Set H.Basic StrictText)) 
-    --           [31000, 26000, 21000, 16000, 11000, 6000, 1000]
+rowsInsertionSubject :: 
+  forall c. (H.Insert c, H.Row c ~ (StrictText, Int)) => 
+  c -> [H.Row c] -> C.Standoff ()
+rowsInsertionSubject _ rows = do
+  C.subject (cs $ show $ length $ rows) $ do
+    liftIO $ do
+      c :: c <- H.new
+      forM_ rows $ H.insertFast c
